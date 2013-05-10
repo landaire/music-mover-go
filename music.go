f8142a53d66402b7b1c4ff04d0d16f0045d3ba57
Lander Brandt <landerbrandt@gmail.com>
2013-05-10 12:59:40 -0700

    Renamed doMove to handleFile, added ID3v2 editing
    I was starting to get pissed off by the fact that I still had to edit my ID3v2 tags for some songs, so I added an extra dependency (see imports) and now the app supports writing ID3v2 title/artist.

--- music.go
+++ music.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/landr0id/go-taglib"
 	"io"
 	"io/ioutil"
 	"os"
@@ -15,6 +16,8 @@ const (
 	MusicPath     = "/Users/lander/ext_music"
 	MaxTransfers  = 5
 )
+
+var regex = regexp.MustCompile(`^(?P<Artist>.*) - (?P<Title>.*)\.mp3`)
 
 func moveFile(source, destination string) error {
 	sourceFile, err := os.Open(source)
@@ -33,7 +36,7 @@ func moveFile(source, destination string) error {
 	return nil
 }
 
-func doMove(c chan string) {
+func handleFile(c chan string) {
 	for name := range c {
 		oldPath := filepath.Join(DownloadsPath, name)
 		newPath := filepath.Join(MusicPath, name)
@@ -48,13 +51,37 @@ func doMove(c chan string) {
 		} else {
 			fmt.Println("Moved", oldPath, "to", newPath)
 		}
+
+		// Check to see if the file has its ID3v2 tags set
+		file, err := taglib.Read(newPath)
+		if err != nil {
+			fmt.Println("Error reading ID3v2 tags: returned nil file struct")
+			continue
+		}
+
+		// Check the title and artist
+		title := file.Title()
+		artist := file.Artist()
+		if title == "" && artist == "" {
+			fmt.Println("Song has no artist or title set in the ID3v2 tags. Setting them accordingly.")
+			// The title and artist weren't present, so let's write them
+			strings := regex.FindStringSubmatch(name)
+			artist := strings[1]
+			title := strings[2]
+			strings = nil
+			fmt.Printf("Title: %s\nArtist: %s\n", title, artist)
+
+			file.SetTitle(title)
+			file.SetArtist(artist)
+			file.Save()
+		}
 	}
 }
 
 func main() {
 	// The channel which will send the found files to the doMove function
 	c := make(chan string, MaxTransfers)
-	go doMove(c)
+	go handleFile(c)
 	fmt.Println("Looking for new files with the extension .mp3 in", DownloadsPath)
 	for {
 		entries, err := ioutil.ReadDir(DownloadsPath)
@@ -65,7 +92,7 @@ func main() {
 		for _, entry := range entries {
 			match, _ := regexp.MatchString(`(?i).*\.mp3$`, entry.Name())
 			if match {
-				fmt.Println("Found file:", entry.Name())
+				fmt.Println("\nFound file:", entry.Name())
 				c <- entry.Name()
 			}
 		}
